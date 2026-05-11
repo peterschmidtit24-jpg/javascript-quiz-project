3dc45516182b6ca1ab9f268cc3d0c8aa5b1f3d24
Galcon95 <peter_schmidt_it@yahoo.de>
2026-05-11 14:21:38 +0200

    data and functionalities for classes question and quiz implemented.

--- src/question.js
+++ src/question.js
@@ -2,6 +2,30 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text
+        this.choices = choices
+        this.answer = answer
+        // difficulty: with 1 being the easiest and 3 being the hardest.
+        this.difficulty = difficulty
+        //console.log("choices = ", choices)
+    }
 
     // 2. shuffleChoices()
+    shuffleChoices() {
+        console.log("choices = ", this.choices)
+
+        for(let i=0; i < this.choices.length; i++) {
+            // console.log("Choise = ", this.choices[i])
+            const j = Math.floor(Math.random() * this.choices.length);
+
+            // swap elements
+            const val = this.choices[i]
+            this.choices[i] = this.choices[j]
+            this.choices[j] = val
+        }
+
+        // console.log("Choise = ", this.choices)
+    }
+
 }
